41116596a1b412a3b264f2b949e82f84037f4ab2
Tomi Lähteenmäki <lihis@lihis.net>
2016-11-17 12:29:00 +0200

    Added AddLaivat.sql
    INSERT INTO's to add ships to Ships table. Github messed up tabs, outside github indentations should be ok.

---
+++ AddLaivat.sql
@@ -0,0 +1,29 @@
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('M/S Wasa Express',	2,				141,			22.81,		4.95,			4150,				17053,				230636000);
+
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('Colombo Express',	1,				335.07,			42.87,		9.80,			103800,				93750,				211433000);
+
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('M/S STAR',			2,				186,			28,			6.5,			4700,				36249,				276672000);
+
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('M/S SUPERSTAR',		2,				177,			27.6,		7,				5000,				36400,				276747000);
+
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('M/S Susanne',		1,				89.8,			13.6,		5.8,			3200,				2409,				244613000);
+
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('Delta Pioneer',		3,				250,			44,			8,				111013,				62320,				240244000);
+
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('Finnstraum',		3,				140,			22,			6.7,			16028,				9956,				257409000);
+
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('Neptune Pioneer',	1,				189.9,			32.26,		8.2,			55921,				31236,				372723000);
+
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('BBC Bahrain',		1,				128.45,			16.6,		6,				7967,				6309,				305847000);
+
+INSERT INTO Ships (ShipName,			ShipTypeID,		ShipLength,		ShipWidth,	ShipDraft,		ShipDeadWeight,		ShipGrossTonnage,	MMSI)
+			VALUES('M/S Amorella',		2,				169.4,			28.2,		6.3,			3690,				34384,				230172000);
