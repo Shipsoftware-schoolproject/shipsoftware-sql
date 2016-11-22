ba68c9240670ca8dd64371a089f15e3f9111cb5a
Tomi Lähteenmäki <lihis@lihis.net>
2016-11-22 13:03:23 +0200

    Kaikki INSERT INTOT:t AddTieto.sql:ssä

--- AddKontit.sql
+++
@@ -1,7 +0,0 @@
-DECLARE @i INT = 1;
-WHILE @i < 16
-BEGIN
-	INSERT INTO Container(ContainerBarCode,	ContainerWeight,	CarryingCapacity,	ContainerWidth,	ContainerHeigth,	ContainerDepth)
-				   VALUES(@i,				2.34,				2000,				244.000,		259.000,			605.000);
-	SET @i = @i + 1;
-END;
