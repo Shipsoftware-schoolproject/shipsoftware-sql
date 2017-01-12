2877eba2522de12e859bd88a8fdf12a2e133ee01
Jori Jalkanen <Jori.jalkanen@gmail.com>
2017-01-12 08:58:23 +0200

    Create HyödyllisiäSQLkyselyitä.sql

---
+++ HyödyllisiäSQLkyselyitä.sql
@@ -0,0 +1,3 @@
+use d6_Shipsoftware
+select ROW_NUMBER() over(order by LogID DESC) as LogNum, LogID,ShipName,ShipID,North,East from lstLaivat where ShipID = 1 order by LogID DESC
+// hakee laivan gps historian uusimmasta-vanhimpaan ja kertoo LogNumissa mikä on uusin ja vanhin vaihda shipID laivaksi jonka haluat löytää
